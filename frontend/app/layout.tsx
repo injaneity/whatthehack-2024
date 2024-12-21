726605450947d26927de9f5832140351176a6a8f
izac <izacwab@gmail.com>
2024-12-22 00:19:49 +0800

    changed header color

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,32 +1,30 @@
-import { ClerkProvider } from '@clerk/nextjs'
-import { neobrutalism } from '@clerk/themes'
-import { Inter } from 'next/font/google'
-import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import { ClerkProvider } from '@clerk/nextjs';
+import { neobrutalism } from '@clerk/themes';
+import './globals.css';
 
 export const metadata = {
   title: 'College Marketplace',
   description: 'Buy, sell, or give away secondhand items for college students',
-}
+};
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
+                                     children,
+                                   }: {
+  children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider
-      appearance={{
-        baseTheme: neobrutalism,
-      }}
-    >
-      <html lang="en">
-        <body className={inter.className}>
+      <ClerkProvider
+          appearance={{
+            baseTheme: neobrutalism,
+          }}
+      >
+        <html lang="en">
+        <body>
         {children}
         </body>
-      </html>
-    </ClerkProvider>
-  )
+        </html>
+      </ClerkProvider>
+  );
 }
+
 
