e349e5171f1cad12c4cb4714ba51438b27093011
Anika Handigol <anikaajayhandigol@gmail.com>
2024-12-21 19:06:50 +0800

    feat:db

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+import models
+from database import SessionLocal, engine
 
 app = FastAPI()
     
 @app.get("/")
 async def health_check():
     return "working"
+
+# Dependency to get a database session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
