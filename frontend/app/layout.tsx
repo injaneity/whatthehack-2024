524d671eaad8eeba6ab9aacf2665bd5523606861
Zane Chee <zane.chee.2023@scis.smu.edu.sg>
2024-12-21 17:22:32 +0800

    feat:sign-in ui

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,8 @@ import { ClerkProvider } from '@clerk/nextjs'
 import { neobrutalism } from '@clerk/themes'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import DotPattern from '@/components/ui/dot-pattern'
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,8 +23,20 @@ export default function RootLayout({
         baseTheme: neobrutalism,
       }}
     >
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
+      <html lang="en"> 
+        <body className={inter.className}>
+          <DotPattern
+          width={20}
+          height={20}
+          cx={1}
+          cy={1}
+          cr={1}
+          className={cn(
+            "[mask-image:linear-gradient(to_bottom_right,white,transparent,transparent)] ",
+          )}
+        />
+        {children}
+      </body>
       </html>
     </ClerkProvider>
   )
--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -6,8 +6,7 @@ export default clerkMiddleware(async (auth) => {
 
 export const config = {
     matcher: [
-      // Protect all routes except '/signin', '_next', and static assets
-      '/((?!signin|_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+      '/((?!sign-in|_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
       '/(api|trpc)(.*)',
     ],
   }
